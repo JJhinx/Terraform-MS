c22c84807414ed1e9801d2425e05a30a51ac4d69
Matteo Sarens <matteosarens@gmail.com>
2024-12-06 00:04:59 +0100

    jenkins test 8 - added credentials to provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,10 @@
 provider "aws" {
   region = var.region
   #sets the provider to aws
+
+  access_key = "AKIAR3HUOTP4GSXHS2L5"
+  secret_key = "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"
+
 }
 
 provider "tls" {

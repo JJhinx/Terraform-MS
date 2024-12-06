f4b486f2290569c61e226209a6fc4a66d262d099
Matteo Sarens <matteosarens@gmail.com>
2024-12-06 11:34:49 +0100

    test 11 - im so done

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "tls_private_key" "Keypair-test" {
   rsa_bits = 2048
 }
 resource "aws_key_pair" "keypair" {
-  key_name = "selfmade"
+  key_name = "selfmade2"
   public_key = tls_private_key.Keypair-test.public_key_openssh
 }
 
@@ -23,19 +23,19 @@ resource "aws_instance" "Ubuntu-Webserver-MS" {
   instance_type = "t2.micro"
   #user_data = file("userdata/Apache-Userdata.sh")
   user_data = data.template_file.userdata-Apache-MS.rendered
-  key_name = "selfmade"
+  key_name = "selfmade2"
   vpc_security_group_ids = [aws_security_group.Security-Group-Allow-SSH-HTTP-MS.id]
   subnet_id = aws_subnet.Subnet-1-MS.id
   associate_public_ip_address = "true"
 
   iam_instance_profile = data.aws_iam_role.FullAccessToS3.name
 
-  count = 2
-
 metadata_options {
   http_endpoint = "enabled"
   http_tokens   = "optional"
   }
+
+  count = 2
 }
 
 #---------------------------------------------------------------
@@ -48,13 +48,11 @@ resource "aws_instance" "MySQL-server-MS" {
   instance_type = "t2.micro"
   #user_data = file("userdata/Mysql-Userdata.tpl")
   user_data = data.template_file.userdata-MySQL-MS.rendered
-  key_name = "selfmade"
+  key_name = "selfmade2"
   vpc_security_group_ids = [aws_security_group.Security-Group-Allow-SSH-HTTP-MS.id]
-  subnet_id = aws_subnet.Subnet-1-MS.id
+  subnet_id = aws_subnet.Subnet-3-MS.id
 
-  private_ip = "192.168.0.200"
-  associate_public_ip_address = "true"
-
+  private_ip = "192.168.2.200"
 metadata_options {
   http_endpoint = "enabled"
   http_tokens   = "optional"
--- provider.tf
+++ provider.tf
@@ -1,10 +1,6 @@
 provider "aws" {
   region = var.region
   #sets the provider to aws
-
-  access_key = "no"
-  secret_key = "no"
-
 }
 
 provider "tls" {

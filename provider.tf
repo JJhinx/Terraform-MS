442ddb28fd1df110f4b8115336953300032a02c3
Matteo Sarens <matteosarens@gmail.com>
2024-12-10 15:23:51 +0100

    i have no clue what's wrong

--- provider.tf
+++ provider.tf
@@ -1,6 +1,8 @@
 provider "aws" {
   region = var.region
   #sets the provider to aws
+  profile = "default"
+  
 }
 
 provider "tls" {
--- variables.tf
+++ variables.tf
@@ -7,6 +7,11 @@ variable "ssh_private_key_file" {
   default = "jenkins-aws.pem"
 }
 
+variable "ssh_key_path" {
+  description = "Path to the SSH private key"
+  type        = string
+}
+
 locals {
   ssh_private_key_content = file(var.ssh_private_key_file)
 }

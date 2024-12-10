bb3b468cb98eeb13fafd7ceb43a20f72afce16ff
Matteo Sarens <matteosarens@gmail.com>
2024-12-10 15:41:03 +0100

    removed previous code that didn't work

--- variables.tf
+++ variables.tf
@@ -2,16 +2,3 @@ variable "region" {
   description = "sets the region variable to ireland"
   default = "eu-west-1"
 }
-
-variable "ssh_private_key_file" {
-  default = "jenkins-aws.pem"
-}
-
-variable "ssh_key_path" {
-  description = "Path to the SSH private key"
-  type        = string
-}
-
-locals {
-  ssh_private_key_content = file(var.ssh_private_key_file)
-}
